9f72b7d087f221850d16474db1abab8c995352a2
armandosantuci <armandodiegoad@gmail.com>
2022-01-03 19:04:57 -0300

    Developed get person by id

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -5,6 +5,7 @@ import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
 
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -34,5 +35,10 @@ public class PersonController {
     @GetMapping
     public List<PersonDTO> listAll(){
         return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
     }
 }
